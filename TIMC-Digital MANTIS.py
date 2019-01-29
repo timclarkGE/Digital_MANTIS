387c8f30c313ea0122bcbd805610277aff96baee
IVUT-DEV-001 <timothy.clark@ge.com>
2019-01-29 14:19:07 -0500

    First working draft. Next step is testing this with a whole tool.

--- TIMC-Digital MANTIS.py
+++ TIMC-Digital MANTIS.py
@@ -7,12 +7,22 @@ import datetime
 import platform
 import sys
 
+from Phidget22.Devices.DCMotor import *
+from Phidget22.Devices.CurrentInput import *
+from Phidget22.Devices.VoltageRatioInput import *
+from Phidget22.Devices.DigitalOutput import *
+from Phidget22.Devices.VoltageOutput import *
+from Phidget22.Net import *
+
 rainbow = ['SteelBlue1', 'DarkGoldenrod1', 'PaleGreen3','LightBlue3','DarkSlateGray3', 'MistyRose3','LightYellow3', 'dark khaki', 'LightSalmon2', 'chocolate1']
 blue_checkers = ['LightSkyBlue1', 'LightSkyBlue3','LightSkyBlue1', 'LightSkyBlue3','LightSkyBlue1', 'LightSkyBlue3','LightSkyBlue1', 'LightSkyBlue3','LightSkyBlue1', 'LightSkyBlue3']
 green_checkers = ['DarkOliveGreen1', 'DarkOliveGreen4','DarkOliveGreen1', 'DarkOliveGreen4','DarkOliveGreen1', 'DarkOliveGreen4','DarkOliveGreen1', 'DarkOliveGreen4','DarkOliveGreen1', 'DarkOliveGreen4']
 green_checkers2 = ['DarkOliveGreen3', 'DarkOliveGreen4','DarkOliveGreen3', 'DarkOliveGreen4','DarkOliveGreen3', 'DarkOliveGreen4','DarkOliveGreen3', 'DarkOliveGreen4','DarkOliveGreen3', 'DarkOliveGreen4']
 gold_checkers = ['gold2', 'gold3','gold2', 'gold3','gold2', 'gold3','gold2', 'gold3','gold2', 'gold3',]
 orange_checkers = ['DarkOrange2', 'DarkOrange4','DarkOrange2', 'DarkOrange4','DarkOrange2', 'DarkOrange4','DarkOrange2', 'DarkOrange4','DarkOrange2', 'DarkOrange4',]
+
+#Line required to look for Phidget devices on the network
+Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICEREMOTE)
 
 class SetupMainWindow:
     def __init__(self):
@@ -27,22 +37,24 @@ class MainWindow:
         self.master.title("Tooling Inspection Motion Controller - Digital MANTIS Electrical")
 
         #Create Frame for Pnematics Control
-        self.out1 = AxisControlFrame(self.master, blue_checkers)
-
-        #class CameraFrame:
-
-        #class AxisFrame:
+        self.out1 = ControlFrame(master, blue_checkers)
 
 class popupWindow(object):
     def __init__(self, master, current_limit, max_velocity, acceleration, invert):
         top = self.top = Toplevel(master)
 
         self.invert = BooleanVar()
+
+        #Store the current settings to the local window variables
+        self.current_limit = current_limit
+        self.max_velocity = max_velocity
+        self.acceleration = acceleration
         self.invert.set(invert)
 
+        #Create the scales
         self.scale_current_limit = Scale(top, orient=HORIZONTAL, from_=2, to=25, resolution=.1,
                            label="Current Limit (A)", length=200)
-        self.scale_max_velocity = Scale(top, orient=HORIZONTAL, from_=1, to=100, resolution=1,
+        self.scale_max_velocity = Scale(top, orient=HORIZONTAL, from_=0.01, to=1, resolution=.01,
                            label="Max Velocity (%)", length=200)
         self.scale_acceleration = Scale(top, orient=HORIZONTAL, from_=0.1, to=100, resolution=0.1,
                            label="Max Acceleration (dty/s^2)", length=200)
@@ -51,7 +63,7 @@ class popupWindow(object):
 
         #Init the window to the current settings
         self.scale_current_limit.set(current_limit)
-        self.scale_max_velocity.set(max_velocity*100)
+        self.scale_max_velocity.set(max_velocity)
         self.scale_acceleration.set(acceleration)
 
         #Grid the widgets
@@ -63,7 +75,7 @@ class popupWindow(object):
 
     def apply_data(self):
         self.current_limit = self.scale_current_limit.get()
-        self.max_velocity = self.scale_max_velocity.get()/100
+        self.max_velocity = self.scale_max_velocity.get()
         self.acceleration = self.scale_acceleration.get()
         self.cleanup()
 
@@ -71,7 +83,7 @@ class popupWindow(object):
         self.top.destroy()
 
 class AxisFrame:
-    def __init__(self, master, initial_name, color):
+    def __init__(self, master, initial_name, move_pos_text, move_neg_text, serial_number, port, color):
         frame = Frame(master, borderwidth=2, relief=SUNKEN, bg=color)
         frame.pack()
         self.master = master
@@ -80,19 +92,19 @@ class AxisFrame:
         self.fontType = "Comic Sans"
 
         #Set the parameters for the axis
-        self.current_limit = 5          #2 to 25A
-        self.max_velocity = 1           #0 to 1
-        self.acceleration = 0.1         #0.1 to 100
+        self.current_limit = 2          #2 to 25A
+        self.max_velocity = .5           #0 to 1
+        self.acceleration = 1         #0.1 to 100
         self.invert = FALSE             #TRUE or FALSE
         self.axis_name = initial_name
-        self.bulkhead_number = 1        #TODO, need to set this as a variable
-        self.move_pos_text  = "POSITIVE"
-        self.move_neg_text = "NEGATIVE"
+        self.move_pos_text  = move_pos_text
+        self.move_neg_text = move_neg_text
+        self.current_text = StringVar()
 
-        self.jog_pos_btn = Button(frame, text=self.move_pos_text, command=lambda: self.jog("+"))
-        self.jog_neg_btn = Button(frame, text=self.move_neg_text, command=lambda: self.jog("-"))
+        self.jog_pos_btn = Button(frame, text=self.move_pos_text)
+        self.jog_neg_btn = Button(frame, text=self.move_neg_text)
         self.configure_btn = Button(frame, text="CONFIGURE", font=(self.fontType,6), command=lambda: self.configure())
-        self.current = Entry(frame, width=5, state=DISABLED)
+        self.current = Entry(frame, width=5, state=DISABLED, textvariable=self.current_text)
         self.speed = Scale(frame, orient=HORIZONTAL, from_=0.01, to=1, resolution=.01, bg=color, label="      Axis Speed", highlightthickness=0)
         self.custom_label = Label(frame, textvariable=self.frame_name, font=(self.fontType, 14), bg=color)
         self.label = Label(frame, text=initial_name, bg=color)
@@ -106,8 +118,49 @@ class AxisFrame:
         self.configure_btn.grid(column=0, row=5, pady=5)
         self.label.grid(column=0, row=6)
 
+        #Connect to Phidget Motor Driver
+        self.axis = DCMotor()
+        self.axis.setDeviceSerialNumber(serial_number)
+        self.axis.setIsHubPortDevice(False)
+        self.axis.setHubPort(port)
+        self.axis.setChannel(0)
+        self.axis.openWaitForAttachment(5000)
+        self.axis.setAcceleration(self.acceleration)
+
+        self.axis_current = CurrentInput()
+        self.axis_current.setDeviceSerialNumber(serial_number)
+        self.axis_current.setIsHubPortDevice(False)
+        self.axis_current.setHubPort(port)
+        self.axis_current.setChannel(0)
+        self.axis_current.openWaitForAttachment(5000)
+        self.axis_current.setDataInterval(200)
+        self.axis_current.setCurrentChangeTrigger(0.0)
+        self.update_current()
+
+        #Bind user button press of jog button to movement method
+        self.jog_pos_btn.bind('<ButtonPress-1>', lambda event: self.jog("+"))
+        self.jog_pos_btn.bind('<ButtonRelease-1>', lambda event: self.jog("0"))
+        self.jog_neg_btn.bind('<ButtonPress-1>', lambda event: self.jog("-"))
+        self.jog_neg_btn.bind('<ButtonRelease-1>', lambda event: self.jog("0"))
+
     def jog(self, direction):
-        print("JOGGING "+direction)
+        #Calculate the speed as a percentage of the maximum velocity
+        velocity = float(self.speed.get())*self.max_velocity
+        #Apply invert if necessary
+        if self.invert == TRUE:
+            velocity *= -1
+
+        #Command Movement
+        if direction == "+":
+            self.axis.setTargetVelocity(velocity)
+        elif direction == "-":
+            self.axis.setTargetVelocity(-1*velocity)
+        elif direction == "0":
+            self.axis.setTargetVelocity(0)
+
+    def update_current(self):
+        self.current_text.set(abs(round(self.axis_current.getCurrent(),3)))
+        root.after(200,self.update_current)
 
     def configure(self):
         #current_limit, max_velocity, acceleration, invert
@@ -122,17 +175,11 @@ class AxisFrame:
         self.acceleration = self.window.acceleration
         self.invert = self.window.invert.get()
 
-        print(self.current_limit, self.max_velocity, self.acceleration, self.invert)
-        #Get the users information and update the axis information: current limit, max speed, acceleration, invert
-        try:
-            if len(self.window.value)>12:
-                self.frame_name.set("SET ERROR")
-            else:
-                self.frame_name.set(self.window.value)
-        except:
-            self.frame_name.set("SET ERROR")
+        #Update Phidget parameters
+        self.axis.setCurrentLimit(self.current_limit)
+        self.axis.setAcceleration(self.acceleration)
 
-class AxisControlFrame:
+class ControlFrame:
     def __init__(self, master, colorArray):
         frame1 = Frame(master, borderwidth=2, relief=SUNKEN)
         frame2 = Frame(master, borderwidth=2, relief=SUNKEN)
@@ -141,24 +188,214 @@ class AxisControlFrame:
         frame5 = Frame(master, borderwidth=2, relief=SUNKEN)
         frame6 = Frame(master, borderwidth=2, relief=SUNKEN)
         frame7 = Frame(master, borderwidth=2, relief=SUNKEN)
+        camera_frame = Frame(master, borderwidth=2, relief=SUNKEN)
 
-        frame1.grid(row=0,column=0)
-        frame2.grid(row=0,column=1)
-        frame3.grid(row=0, column=2)
-        frame4.grid(row=0, column=3)
-        frame5.grid(row=0, column=4)
-        frame6.grid(row=0, column=5)
-        frame7.grid(row=0, column=6)
+        frame1.grid(row=1,column=0)
+        frame2.grid(row=1,column=1)
+        frame3.grid(row=1, column=2)
+        frame4.grid(row=1, column=3)
+        frame5.grid(row=1, column=4)
+        frame6.grid(row=1, column=5)
+        frame7.grid(row=1, column=6)
+        camera_frame.grid(row=3,column=0, columnspan=7, sticky=W)
 
 
-        out1 = AxisFrame(frame1, "BASE CIRC", colorArray[0])
-        out2 = AxisFrame(frame2, "BASE AUX", colorArray[1])
-        out3 = AxisFrame(frame3, "VARD VERT", colorArray[2])
-        out4 = AxisFrame(frame4, "VARD ROT", colorArray[3])
-        out5 = AxisFrame(frame5, "DA MAST", colorArray[4])
-        out6 = AxisFrame(frame6, "DA PAN", colorArray[5])
-        out7 = AxisFrame(frame7, "DA TILT", colorArray[6])
+        out1 = AxisFrame(frame1, "BASE CIRC", "POSITIVE", "NEGATIVE", 539552, 2, colorArray[0])
+        out2 = AxisFrame(frame2, "BASE AUX", "POSITIVE", "NEGATIVE", 538798, 0, colorArray[1])
+        out3 = AxisFrame(frame3, "VARD ROT", "POSITIVE", "NEGATIVE", 539552, 0, colorArray[2])
+        out4 = AxisFrame(frame4, "VARD VERT", "POSITIVE", "NEGATIVE", 539552, 1, colorArray[3])
+        out5 = AxisFrame(frame5, "DA MAST", "POSITIVE", "NEGATIVE", 539552, 3, colorArray[4])
+        out6 = AxisFrame(frame6, "DA PAN", "POSITIVE", "NEGATIVE", 539552, 5, colorArray[5])
+        out7 = AxisFrame(frame7, "DA TILT", "POSITIVE", "NEGATIVE", 539552, 4, colorArray[6])
 
+        #RJ Camera Control Code
+        self.invert_tilt = BooleanVar()
+        self.invert_pan = BooleanVar()
+        self.btn_power = Button(camera_frame, text="PWR", font="Courier, 12", command=self.toggle_power)
+        self.btn_near = Button(camera_frame, text="NEAR", font="Courier, 12", width=7)
+        self.btn_far = Button(camera_frame, text="FAR", font="Courier, 12", width=7)
+        self.btn_wide = Button(camera_frame, text="WIDE", font="Courier, 12", width=7)
+        self.btn_tele = Button(camera_frame, text="TELE", font="Courier, 12", width=7)
+        self.btn_ms = Button(camera_frame, text="MS", font="Courier, 12", width=7)
+        self.left_light_scale = Scale(camera_frame, orient=VERTICAL, from_=0, to=0.22, resolution=0.01, command=self.update_left_intensity)
+        self.right_light_scale = Scale(camera_frame, orient=VERTICAL, from_=0, to=0.22, resolution=0.01, command=self.update_right_intensity)
+        self.label_lights = Label(camera_frame, text="  Light Intensity")
+        self.btn_tilt_up = Button(camera_frame, text="TILT UP", font="Courier, 12", width=10)
+        self.btn_tilt_down = Button(camera_frame, text="TILT DOWN", font="Courier, 12", width=10)
+        self.btn_pan_right = Button(camera_frame, text="PAN RIGHT", font="Courier, 12", width=10)
+        self.btn_pan_left = Button(camera_frame, text="PAN LEFT", font="Courier, 12", width=10)
+        self.tilt_speed = Scale(camera_frame, orient=HORIZONTAL, from_=0.01, to=.22, resolution=0.01)
+        self.pan_speed = Scale(camera_frame, orient=HORIZONTAL, from_=0.01, to=.22, resolution=0.01)
+        self.ckbx_invert_tilt = Checkbutton(camera_frame, text="Invert Tilt", variable=self.invert_tilt)
+        self.ckbx_invert_pan = Checkbutton(camera_frame, text="Invert Pan", variable=self.invert_pan)
+
+        self.btn_power.grid(row=0, column=0, padx=60)
+        self.btn_ms.grid(row=1, column=0, padx=5, pady=5)
+        self.btn_near.grid(row=0, column=2, padx=10, pady=10)
+        self.btn_far.grid(row=1, column=2, padx=10, pady=10)
+        self.btn_tele.grid(row=0, column=3, padx=10, pady=10)
+        self.btn_wide.grid(row=1, column=3, padx=10, pady=10)
+        self.left_light_scale.grid(row=0, column=4, rowspan=3, padx=20, pady=5)
+        self.right_light_scale.grid(row=0, column=5, rowspan=3, padx=20, pady=5)
+        self.label_lights.grid(row=3, column=4, columnspan=2, sticky=N)
+        self.btn_tilt_up.grid(row=0, column=6, padx=20, pady=5)
+        self.btn_tilt_down.grid(row=1, column=6, padx=20, pady=5)
+        self.btn_pan_right.grid(row=0, column=7, padx=20, pady=5)
+        self.btn_pan_left.grid(row=1, column=7, padx=20, pady=5)
+        self.tilt_speed.grid(row=2, column=6)
+        self.pan_speed.grid(row=2, column=7)
+        self.ckbx_invert_tilt.grid(row=3,column=6)
+        self.ckbx_invert_pan.grid(row=3, column=7)
+
+        #Connect to Phidget Devices
+        self.power = DigitalOutput()
+        self.power.setDeviceSerialNumber(538798)
+        self.power.setIsHubPortDevice(False)
+        self.power.setHubPort(1)
+        self.power.setChannel(0)
+        self.power.openWaitForAttachment(5000)
+
+        self.manual_select = DigitalOutput()
+        self.manual_select.setDeviceSerialNumber(538798)
+        self.manual_select.setIsHubPortDevice(False)
+        self.manual_select.setHubPort(1)
+        self.manual_select.setChannel(1)
+        self.manual_select.openWaitForAttachment(5000)
+
+        self.near = DigitalOutput()
+        self.near.setDeviceSerialNumber(538798)
+        self.near.setIsHubPortDevice(False)
+        self.near.setHubPort(1)
+        self.near.setChannel(2)
+        self.near.openWaitForAttachment(5000)
+
+        self.far = DigitalOutput()
+        self.far.setDeviceSerialNumber(538798)
+        self.far.setIsHubPortDevice(False)
+        self.far.setHubPort(1)
+        self.far.setChannel(3)
+        self.far.openWaitForAttachment(5000)
+
+        self.wide = DigitalOutput()
+        self.wide.setDeviceSerialNumber(538798)
+        self.wide.setIsHubPortDevice(False)
+        self.wide.setHubPort(1)
+        self.wide.setChannel(4)
+        self.wide.openWaitForAttachment(5000)
+
+        self.tele = DigitalOutput()
+        self.tele.setDeviceSerialNumber(538798)
+        self.tele.setIsHubPortDevice(False)
+        self.tele.setHubPort(1)
+        self.tele.setChannel(5)
+        self.tele.openWaitForAttachment(5000)
+
+        self.left_light = VoltageOutput()
+        self.left_light.setDeviceSerialNumber(538798)
+        self.left_light.setIsHubPortDevice(False)
+        self.left_light.setHubPort(2)
+        self.left_light.setChannel(0)
+        self.left_light.openWaitForAttachment(5000)
+
+        self.right_light = VoltageOutput()
+        self.right_light.setDeviceSerialNumber(538798)
+        self.right_light.setIsHubPortDevice(False)
+        self.right_light.setHubPort(3)
+        self.right_light.setChannel(0)
+        self.right_light.openWaitForAttachment(5000)
+
+        self.pan = VoltageOutput()
+        self.pan.setDeviceSerialNumber(538798)
+        self.pan.setIsHubPortDevice(False)
+        self.pan.setHubPort(5)
+        self.pan.setChannel(0)
+        self.pan.openWaitForAttachment(5000)
+
+        self.tilt = VoltageOutput()
+        self.tilt.setDeviceSerialNumber(538798)
+        self.tilt.setIsHubPortDevice(False)
+        self.tilt.setHubPort(4)
+        self.tilt.setChannel(0)
+        self.tilt.openWaitForAttachment(5000)
+
+
+        self.btn_near.bind('<ButtonPress-1>', lambda event: self.focus("+"))
+        self.btn_near.bind('<ButtonRelease-1>', lambda event: self.focus("0"))
+        self.btn_far.bind('<ButtonPress-1>', lambda event: self.focus("-"))
+        self.btn_far.bind('<ButtonRelease-1>', lambda event: self.focus("0"))
+        self.btn_wide.bind('<ButtonPress-1>', lambda event: self.zoom("-"))
+        self.btn_wide.bind('<ButtonRelease-1>', lambda event: self.zoom("0"))
+        self.btn_tele.bind('<ButtonPress-1>', lambda event: self.zoom("+"))
+        self.btn_tele.bind('<ButtonRelease-1>', lambda event: self.zoom("0"))
+        self.btn_ms.bind('<ButtonPress-1>', lambda event: self.focus_type("ON"))
+        self.btn_ms.bind('<ButtonRelease-1>', lambda event: self.focus_type("OFF"))
+
+        self.btn_tilt_up.bind('<ButtonPress-1>', lambda event: self.tilt_move("+"))
+        self.btn_tilt_up.bind('<ButtonRelease-1>', lambda event: self.tilt_move("0"))
+        self.btn_tilt_down.bind('<ButtonPress-1>', lambda event: self.tilt_move("-"))
+        self.btn_tilt_down.bind('<ButtonRelease-1>', lambda event: self.tilt_move("0"))
+        self.btn_pan_right.bind('<ButtonPress-1>', lambda event: self.pan_move("R"))
+        self.btn_pan_right.bind('<ButtonRelease-1>', lambda event: self.pan_move("0"))
+        self.btn_pan_left.bind('<ButtonPress-1>', lambda event: self.pan_move("L"))
+        self.btn_pan_left.bind('<ButtonRelease-1>', lambda event: self.pan_move("0"))
+
+    def toggle_power(self):
+        if self.power.getState() == True:
+            self.power.setState(False)
+        elif self.power.getState() == False:
+            self.power.setState(True)
+
+    def update_left_intensity(self, val):
+        self.left_light.setVoltage(float(val))
+
+    def update_right_intensity(self, val):
+        self.right_light.setVoltage(float(val))
+
+    def focus(self, direction):
+        if direction == "+":
+            self.near.setState(TRUE)
+        elif direction == "-":
+            self.far.setState(TRUE)
+        elif direction == "0":
+            self.far.setState(FALSE)
+            self.near.setState(FALSE)
+
+    def zoom(self, direction):
+        if direction == "+":
+            self.tele.setState(TRUE)
+        elif direction == "-":
+            self.wide.setState(TRUE)
+        elif direction == "0":
+            self.tele.setState(FALSE)
+            self.wide.setState(FALSE)
+
+    def pan_move(self, direction):
+        voltage = float(self.pan_speed.get())
+        if self.invert_pan.get():
+            voltage *= -1
+        if direction == "R":
+            self.pan.setVoltage(voltage)
+        elif direction == "L":
+            self.pan.setVoltage(-1*voltage)
+        elif direction == "0":
+            self.pan.setVoltage(0)
+
+    def tilt_move(self, direction):
+        voltage = float(self.tilt_speed.get())
+        if self.invert_tilt.get():
+            voltage *= -1
+        if direction == "+":
+            self.tilt.setVoltage(voltage)
+        elif direction == "-":
+            self.tilt.setVoltage(-1*voltage)
+        elif direction == "0":
+            self.tilt.setVoltage(0)
+
+    def focus_type(self, state):
+        if state == "ON":
+            self.manual_select.setState(True)
+        elif state == "OFF":
+            self.manual_select.setState(False)
 
 root = Tk()
 TIMC = MainWindow(root, SetupMainWindow())
